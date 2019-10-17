39d250d844ad06fa2d28b84ac1ca7adb18bccbc2
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-17 14:00:02 -0700

    knex set up

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-
 const server = express()
+
 
 server.use(express.json())
 
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'up' });
+    res.status(200).json('Hello there');
 });
 
 
