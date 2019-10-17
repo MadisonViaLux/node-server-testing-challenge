160dcb8673e7a0a4a908e25f0259968ff8fc9fac
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-17 14:29:17 -0700

    set up routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const server = express()
+const Users = require('./users/user-router')
 
 
 server.use(express.json())
+server.use('/api', Users)
 
 
 server.get('/', (req, res) => {
